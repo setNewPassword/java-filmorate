04cd7f96c8d99d9fc764c904a33008210a5ade6d
Dee <dima.irqtsk@yandex.ru>
2023-04-06 19:05:43 +0800

    10th sprint, 1st review, little fix #3

--- src/main/java/ru/yandex/practicum/filmorate/service/UserService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/UserService.java
@@ -6,11 +6,18 @@ import java.util.List;
 
 public interface UserService {
     List<User> getAllUsers();
+
     User create(User user);
+
     User update(User user);
+
     User addFriend(Long basicUserId, Long addingUserId);
+
     User removeFriend(Long basicUserId, Long removingUserId);
+
     List<User> getCommonFriends(Long basicUserId, Long secondUserId);
+
     User getUserById(Long userId);
+
     List<User> getUsersFriends(Long userId);
 }
