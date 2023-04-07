9f782ba44b6680e1f60da4b05bf22955061e3d38
Dee <dima.irqtsk@yandex.ru>
2023-04-07 13:42:29 +0800

    10th sprint, 2nd review, little fix

--- src/main/java/ru/yandex/practicum/filmorate/service/FilmServiceImpl.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/FilmServiceImpl.java
@@ -4,10 +4,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import ru.yandex.practicum.filmorate.exception.FilmNotFoundException;
-import ru.yandex.practicum.filmorate.exception.UserNotFoundException;
 import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.storage.FilmStorage;
-import ru.yandex.practicum.filmorate.storage.UserStorage;
 
 import java.util.List;
 import java.util.stream.Collectors;
