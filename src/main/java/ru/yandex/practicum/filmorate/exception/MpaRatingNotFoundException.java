43e784e93dbc167a60dac10532da64ef7c4336d1
Dee <dima.irqtsk@yandex.ru>
2023-05-08 15:56:11 +0800

    11th sprint, 1st review, style fix

--- src/main/java/ru/yandex/practicum/filmorate/exception/MpaRatingNotFoundException.java
+++ src/main/java/ru/yandex/practicum/filmorate/exception/MpaRatingNotFoundException.java
@@ -1,6 +1,6 @@
 package ru.yandex.practicum.filmorate.exception;
 
-public class MpaRatingNotFoundException extends EntityNotFoundException{
+public class MpaRatingNotFoundException extends EntityNotFoundException {
     public MpaRatingNotFoundException(String message) {
         super(message);
     }
